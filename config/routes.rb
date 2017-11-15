3d22d7110a9180380b70f0fc0da3b68e175b3110
ward <vakpko@gmail.com>
2017-11-15 14:55:43 +0100

    home page with categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'pages#home' 
+  root to: 'categories#index'
+
+  resources :categories
 
   devise_for :users
 
   resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :categories, only: [:show]
 end
