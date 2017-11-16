55bee3486088ed2ffa96e8c435a9a8004d1d66e8
Luci-Lawless <lu.gmmm@gmail.com>
2017-11-16 17:55:26 +0100

    Create show action for categories.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,20 @@
 class CategoriesController < ApplicationController
+  before_action :set_category
+
   def index
     @categories = Category.all
   end
 
   def show
     @category = Category.find(params[:id])
+    @pets = @category.pets
   end
 
+
+  private
+
+    def set_category
+      @category = Category(params[:id])
+
+    end
 end
